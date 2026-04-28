43fb0f9d06820a570f60508227e7bcfc1b24d9b9
Yudistio Izza <137758510+Yudistioizza@users.noreply.github.com>
2026-04-28 21:49:54 +0700

    fix: ganti nama github repo

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 import { resolve } from 'path'
 
 export default defineConfig({
-  base: '/portofolio-v3/',
+  base: '/portofolio/',
   plugins: [
     react(),
     tailwindcss(),
